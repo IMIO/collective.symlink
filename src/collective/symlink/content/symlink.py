9b01e06e0e5ba75880995f03c63f884b732c6336
Martin Peeters <martin.peeters@affinitic.be>
2019-08-07 13:08:36 +0200

    Fix acquisition issues on symlink object

--- src/collective/symlink/content/symlink.py
+++ src/collective/symlink/content/symlink.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from Acquisition import aq_base, aq_inner, aq_parent
 from collective.symlink import _
 from plone.dexterity.browser.view import DefaultView
 from plone.dexterity.content import Container
@@ -7,6 +8,10 @@ from plone.formwidget.contenttree import ObjPathSourceBinder
 from plone.supermodel import model
 from z3c.relationfield.schema import RelationChoice
 from zope.interface import implements
+
+import types
+
+_marker = object()
 
 
 class ISymlink(model.Schema):
@@ -18,25 +23,58 @@ class ISymlink(model.Schema):
 class Symlink(Container):
     implements(ISymlink)
 
+    cmf_uid = None
+
     def __call__(self):
-        link_obj = self.symbolic_link.to_object
-        template = link_obj.unrestrictedTraverse(link_obj.getLayout())
-        template.context = AdaptedContext(self)
+        template = self._link.unrestrictedTraverse(self._link.getLayout())
+        template.context = self
         return template()
 
+    def Title(self):
+        """Delegated title
+        """
+        link = self._link
+        if link is not None:
+            return aq_inner(link).Title()
 
-class AdaptedContext(object):
-    _not_inherited_attrs = ("id", "absolute_url", "__parent__")
-
-    def __init__(self, symlink):
-        self._symlink = symlink
-        self._original_object = symlink.symbolic_link.to_object
+    @property
+    def _link(self):
+        if 'symbolic_link' not in self.__dict__:
+            return None
+        return self.__getattribute__('symbolic_link').to_object
 
     def __getattr__(self, key):
-        if key in self._not_inherited_attrs:
-            return getattr(self._symlink, key)
-        else:
-            return getattr(self._original_object, key)
+        # Inspired by collective.alias
+        if (
+            key.startswith("_v_")
+            or key.startswith("_p_")
+            or key.endswith("_Permission")
+        ):
+            raise AttributeError(key)
+
+        link = self._link
+        if link is None:
+            return super(Symlink, self).__getattr__(key)
+
+        link = aq_inner(link)
+
+        if not hasattr(aq_base(link), key):
+            return super(Symlink, self).__getattr__(key)
+
+        link_attr = getattr(link, key, _marker)
+
+        if link_attr is _marker:
+            return super(Symlink, self).__getattr__(key)
+
+        # if this is an acquisition wrapped object, re-wrap it in the alias
+        if aq_parent(link_attr) is link:
+            link_attr = aq_base(link_attr).__of__(self)
+
+        # if it is a bound method, re-bind it so that im_self is the alias
+        if isinstance(link_attr, types.MethodType):
+            return types.MethodType(link_attr.im_func, self, type(self))
+
+        return link_attr
 
 
 class SymlinkView(DefaultView):
