9701caa7b41e6c25d316cd03c7b592019019e5c7
fngaha <franck.o.ngaha@gmail.com>
2020-04-12 22:07:55 +0200

    Add a marker interface ISymlinkable
    PRJ-423
    this interface defines a content type who is allowable by a symlink.
    And subscribe to element_modified

--- src/collective/symlink/interfaces.py
+++ src/collective/symlink/interfaces.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
+from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
 class ICollectiveSymlinkLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+class ISymlinkable(Interface):
+    """Marker interface that defines a content type who is allowable by a symlink."""
