75893453feffb88bc265dd9c145b6b03596c027b
fngaha <franck.o.ngaha@gmail.com>
2020-04-13 17:03:37 +0200

    Undo Add a marker interface ISymlinkable
    PRJ-423

--- src/collective/symlink/interfaces.py
+++ src/collective/symlink/interfaces.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
-from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
 class ICollectiveSymlinkLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
-
-class ISymlinkable(Interface):
-    """Marker interface that defines a content type who is allowable by a symlink."""
